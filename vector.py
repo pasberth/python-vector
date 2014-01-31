f8386a18022982bdb65fc729fdf9deafd60a639b
UTAGAWA Kiki <utagawa.kiki@gmail.com>
2014-01-31 23:29:55 +0900

    Thanks @pasberth

--- vector.py
+++ vector.py
@@ -8,7 +8,7 @@ class vector:
     def __add__(self, o):
         if len(o.v) < len(self.v): o.v += [0 for x in range(len(self.v)-len(o.v))]
         elif len(o.v) > len(self.v): self.v += [0 for x in range(len(o.v)-len(self.v))]
-        return [self.v[i] + o.v[i] for i in range(len(o.v))]
+        return vector([self.v[i] + o.v[i] for i in range(len(o.v))])
 
     def __sub__(self, o):
         return self.__add__(vector(o*-1))
